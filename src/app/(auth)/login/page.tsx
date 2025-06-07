97077af35e32c34806d36195a077342b8dc133f9
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-07 13:08:47 +0200

    add authentication routes & pages layouts

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <p>Please enter your credentials to log in.</p>
+      {/* Add your login form here */}
+    </>
+  )
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <>
+      <h1>Register Page</h1>
+      <p>Please fill out the form to create a new account.</p>
+      {/* Add your registration form here */}
+    </>
+  )
+}
