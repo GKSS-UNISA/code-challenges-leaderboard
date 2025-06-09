e748c13ebff94352604c7f903ee79255bab0ea3b
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-09 16:45:02 +0200

    add global navbar

---
+++ src/components/Navbar/config.ts
@@ -0,0 +1,21 @@
+const config = {
+    homeUrl: '/',
+    logo: {
+        name: 'GKSS - UNISA',
+        img: {
+            src: '/logo.png',
+            alt: 'GKSS - UNISA Logo',
+        },
+    },
+    mobileLinks: [
+        { title: 'Documentation', href: '/documentation', isLink: true },
+        { title: 'Challenges', href: 'https://github.com/GKSS-UNISA/code-challenges/', isLink: true },
+        { title: 'Source', href: 'https://github.com/GKSS-UNISA/code-challenges-leaderboard/', isLink: true },
+    ],
+    actions: [
+        { title: 'Sign in', href: '/login', isButton: false },
+        { title: 'Sign Up', href: '/register', isButton: true, },
+    ]
+}
+
+export default config;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,135 @@
+'use client';
+
+import { Menu } from "lucide-react";
+import { ReactNode } from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Navbar as NavbarComponent,
+  NavbarLeft,
+  NavbarRight,
+} from "@/components/ui/navbar";
+import Navigation from "@/components/ui/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Link from "next/link";
+import Image from "next/image";
+import config from './config'
+import { title } from "process";
+
+interface NavbarLink {
+  text: string;
+  href: string;
+}
+
+interface NavbarActionProps {
+  text: string;
+  href: string;
+  // variant?: ButtonProps["variant"];
+  icon?: ReactNode;
+  iconRight?: ReactNode;
+  isButton?: boolean;
+}
+
+interface NavbarProps {
+  logo?: ReactNode;
+  name?: string;
+  homeUrl?: string;
+  mobileLinks?: NavbarLink[];
+  actions?: NavbarActionProps[];
+  showNavigation?: boolean;
+  customNavigation?: ReactNode;
+  className?: string;
+}
+
+export default function Navbar({
+  showNavigation = true,
+  customNavigation,
+  className,
+}: NavbarProps) {
+  return (
+    <header className={cn("sticky top-0 z-50 -mb-4 px-4 pb-4", className)}>
+      <div className="fade-bottom bg-background/15 absolute left-0 h-24 w-full backdrop-blur-lg"></div>
+      <div className="max-w-container relative mx-auto">
+        <NavbarComponent>
+          <NavbarLeft>
+            <Link
+              href={config.homeUrl}
+              className="flex items-center gap-2 text-xl font-bold"
+            >
+              <Image width={40} height={40} src={config.logo.img.src} alt={config.logo.img.alt} className='rounded-full' />
+              {config.logo.name}
+            </Link>
+
+            {showNavigation && (customNavigation || <Navigation
+              logoHref="/"
+              menuItems={[
+                ...config.mobileLinks.map((link, i) => ({
+                  title: link.title,
+                  href: link.href,
+                  isLink: true,
+                }))
+              ]}
+             />)}
+          </NavbarLeft>
+
+          <NavbarRight>
+            {config.actions.map((action, index) =>
+              action.isButton ? (
+                <Button
+                  key={index}
+                  asChild
+                >
+                  <Link href={action.href}>
+                    {action.title}
+                  </Link>
+                </Button>
+              ) : (
+                <Link
+                  key={index}
+                  href={action.href}
+                  className="hidden text-sm md:block"
+                >
+                  {action.title}
+                </Link>
+              ),
+            )}
+
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="shrink-0 md:hidden"
+                >
+                  <Menu className="size-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+
+              <SheetContent side="right">
+                <nav className="grid gap-6 text-lg font-medium">
+                  <Link
+                    href={config.homeUrl}
+                    className="flex items-center gap-2 text-xl font-bold"
+                  >
+                    <span>{config.logo.name}</span>
+                  </Link>
+
+                  {config.mobileLinks.map((link, index) => (
+                    <Link
+                      key={index}
+                      href={link.href}
+                      className="text-muted-foreground hover:text-foreground"
+                    >
+                      {link.title}
+                    </Link>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </NavbarRight>
+        </NavbarComponent>
+      </div>
+    </header>
+  );
+}
