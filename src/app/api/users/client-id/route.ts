47035f09bc4427a6b577cc8083211a3150f9e064
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-11 12:07:51 +0200

    add sign in page

--- src/app/api/users/client-id/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  // Generate a random client ID value for current user
-}
-
-export async function GET() {
-  // Read the current user's client ID.
-}
