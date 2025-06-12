bf90cdb370d19c7b439a2789a06644bd7fccc8dc
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-12 10:25:38 +0200

    update section import paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Hero from "@/components/sections/Hero";
-import Dashboard from "@/components/sections/Dashboard";
+import Hero from "@/components/sections/hero";
+import Dashboard from "@/components/sections/dashboard";
 
 export default function Index() {
   const isAuthenticated = true; // Replace with actual authentication logic
