77c3783472fa55bbeccbe5b35c3ee7ecffa366b4
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-12 09:54:56 +0200

    add user registration form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Register() {
   return (
     <>
-      <h1>Register Page</h1>
-      <p>Please fill out the form to create a new account.</p>
-      {/* Add your registration form here */}
+      <SignUp />
     </>
   );
 }
