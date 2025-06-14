13bc3d6a3fb4216ed8305387775e256fa42b4d04
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-14 14:46:55 +0200

    add auth client

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@/generated/prisma";
+import { createAuthClient } from "better-auth/react";
 
 const prisma = new PrismaClient();
 
@@ -8,4 +9,9 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
 });
+
+export const authClient = createAuthClient({});
