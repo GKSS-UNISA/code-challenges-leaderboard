e14daf8ee8c43c9a8af3b74cb9023cfe6879ebf6
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-14 13:03:09 +0200

    add global navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navbar from "@/components/navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <main className="w-full min-h-[calc(100vh-96px)] mt-6">{children}</main>
       </body>
     </html>
   );
