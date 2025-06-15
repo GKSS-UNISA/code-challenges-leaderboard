e25845924f10f6318022499553720084e526b960
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-15 11:36:46 +0200

    add lint-staged test run script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
+// Generate next lint command for lint-staged
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
@@ -5,8 +6,26 @@ const buildEslintCommand = (filenames) =>
 
 /**@type {import('lint-staged').Configuration} */
 const lintStagedConfig = {
-  "*.{js,ts,tsx}": [buildEslintCommand, "prettier --write"],
-  "*.{json,yml,yaml,md,mdx,html}": ["prettier --write"],
+  // Run ESLint and Prettier on staged JavaScript and TypeScript files
+  "*.{js,ts,tsx}": { title: [buildEslintCommand, "prettier --write"] },
+
+  // Run tests for changed typescript files
+  // If the file is a test file, run it directly
+  // otherwise, run related tests
+  "*.{ts,tsx}": {
+    title: "Running tests for changed files",
+    task: (files) =>
+      files.map((f) => {
+        if (f.endsWith(".test.ts") || f.endsWith(".test.tsx")) {
+          return `NODE_ENV=test jest ${process.cwd()}/src/${f}`;
+        } else {
+          return `NODE_ENV=test jest --bail --findRelatedTests`;
+        }
+      }),
+  },
+
+  // Run prettier on staged JSON, YAML, Markdown and prisma files
+  "*.{json,yml,yaml,md,mdx}": ["prettier --write"],
   "*.prisma": ["prettier --write"],
 };
 
