74224f684cdc0139798d104aefdb5f5065355807
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-15 15:09:49 +0200

    add jest eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
+    overrides: [
+      {
+        plugins: ["jest"],
+        extends: ["plugin:jest/recommended", "plugin:jest/style"],
+        files: ["__tests__/**/*", "*.test.{ts,tsx}"],
+        rules: {
+          "jest/consistent-test-it": ["error"],
+          "jest/no-commented-out-tests": ["error"],
+          "jest/prefer-comparison-matcher": "error",
+          "jest/prefer-equality-matcher": "error",
+          "jest/no-test-return-statement": "error",
+        },
+      },
+    ],
     ignorePatterns: ["src/generated/prisma"],
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
   }),
