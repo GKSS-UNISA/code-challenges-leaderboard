efecb319df9f9cac34a1cdfb766e2f38952c4ea1
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-16 17:23:16 +0200

    add user login functionality

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -10,51 +10,137 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { useForm } from "@tanstack/react-form";
+import FieldInfo from "@/components/ui/form-info";
 import { Label } from "@/components/ui/label";
+import { authClient } from "@/lib/auth";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function LoginForm() {
+  const router = useRouter();
+
+  const form = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async ({ value }) => {
+      try {
+        const resp = await authClient.signIn.email({
+          email: value.email,
+          password: value.password,
+        });
+
+        if (resp.error) {
+          console.error("Login error:", resp.error);
+          return;
+        }
+
+        console.log("Login successful:", resp.data);
+        form.reset();
+        router.push("/dashboard");
+      } catch (error) {
+        console.error("Error during login:", error);
+      }
+    },
+  });
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+    form.handleSubmit();
+  }
+
   return (
     <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle>Login to your account</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
+      <form onSubmit={handleSubmit}>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <div className="flex flex-col gap-6 mt-4">
             <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
+              <form.Field
+                name="email"
+                validators={{
+                  onChange: ({ value }) =>
+                    !value
+                      ? "Email is required"
+                      : !value.includes("@")
+                        ? "Invalid email"
+                        : undefined,
+                }}
+              >
+                {(field) => (
+                  <>
+                    <Label htmlFor={field.name}>Email</Label>
+                    <Input
+                      id={field.name}
+                      type="email"
+                      placeholder="your@example.com"
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      value={field.state.value}
+                      required
+                    />
+                    <FieldInfo field={field} />
+                  </>
+                )}
+              </form.Field>
             </div>
+
             <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" required />
+              <form.Field
+                name="password"
+                validators={{
+                  onChange: ({ value }) =>
+                    !value ? "Password is required" : undefined,
+                }}
+              >
+                {(field) => (
+                  <>
+                    <Label htmlFor={field.name}>Password</Label>
+                    <Input
+                      id={field.name}
+                      type="password"
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      value={field.state.value}
+                      required
+                    />
+                    <FieldInfo field={field} />
+                  </>
+                )}
+              </form.Field>
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Login
-        </Button>
-        <p className="text-center text-xs text-muted-foreground mt-2">
-          Don&apos;t have an account?{" "}
-          <Link
-            href="/register"
-            className="hover:underline hover:text-foreground"
+        </CardContent>
+
+        <CardFooter className="flex-col gap-2 mt-4">
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
           >
-            Create one.
-          </Link>
-        </p>
-      </CardFooter>
+            {([canSubmit, isSubmitting]) => (
+              <Button type="submit" className="w-full" disabled={!canSubmit}>
+                {isSubmitting ? "Logging in..." : "Login"}
+              </Button>
+            )}
+          </form.Subscribe>
+
+          <p className="text-center text-xs text-muted-foreground mt-2">
+            Don&apos;t have an account?{" "}
+            <Link
+              href="/register"
+              className="hover:underline hover:text-foreground"
+            >
+              Sign up.
+            </Link>
+          </p>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
