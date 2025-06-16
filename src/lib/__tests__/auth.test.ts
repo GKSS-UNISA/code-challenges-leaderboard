40a7f3080d0b282dc398d22e598dd9220104b319
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-16 12:09:16 +0200

    remove unused test folder

--- src/lib/__tests__/auth.test.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from "../auth";
-
-describe("auth", () => {
-  it("exports auth object with api property", () => {
-    expect(auth).toBeDefined();
-    expect(auth).toHaveProperty("api");
-  });
-});
