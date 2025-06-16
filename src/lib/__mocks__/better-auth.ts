86e068c3f0bb5d3513130995d18404259cf86a2e
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-16 02:02:33 +0200

    remove unused mock files

--- src/lib/__mocks__/better-auth.ts
+++
@@ -1,9 +0,0 @@
-export const betterAuth = jest.fn().mockImplementation(() => ({
-  api: {},
-}));
-
-export const createAuthClient = jest.fn().mockReturnValue({});
-
-export const nextCookies = jest.fn().mockReturnValue({});
-
-export const prismaAdapter = jest.fn().mockReturnValue({});
