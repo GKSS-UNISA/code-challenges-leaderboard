67709589a598f138a90e1f82868abd66094cb0e1
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-18 00:25:26 +0200

    hotfix: update cookie name

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,6 +8,7 @@ import { nextCookies } from "better-auth/next-js";
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
   advanced: {
     cookies: {
       session_token: {
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { type NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
+  const sessionCookie = getSessionCookie(request, {
+    cookieName: process.env.BETTER_AUTH_COOKIE_NAME,
+  });
   if (!sessionCookie)
     return NextResponse.redirect(new URL("/login", request.url));
 
