d22f3998b6ce903aaa18f2d24b22e72eb0397cc5
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-18 17:00:05 +0200

    update tests

--- src/lib/__tests__/utils.test.ts
+++ src/lib/__tests__/utils.test.ts
@@ -1,11 +1,5 @@
-import { describe, test, expect, vi } from "vitest";
 import { cn } from "../utils";
 
 test("cn function should be defined", () => {
   expect(cn).toBeDefined();
-});
-
-test("cn should merge class names", () => {
-  const result = cn("class1", "class2", { class3: true, class4: false });
-  expect(result).toBe("class1 class2 class3");
 });
