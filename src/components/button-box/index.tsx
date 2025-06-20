be2e6e412905f3b5c1ffa58fce2f14be2885674d
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-21 00:31:28 +0200

    add button box component

---
+++ src/components/button-box/index.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import * as React from "react";
+import { authClient } from "@/lib/auth";
+import { Button } from "../ui/button";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function ButtonBox() {
+  const [isHome, setIsHome] = React.useState(false);
+
+  const { data: session } = authClient.useSession();
+
+  const pathname = usePathname();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (pathname === "/home") {
+      setIsHome(true);
+    } else {
+      setIsHome(false);
+    }
+  }, [pathname]);
+
+  async function handleSignOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/home");
+        },
+      },
+    });
+  }
+
+  return (
+    <>
+      {!session && (
+        <>
+          <Button asChild>
+            <Link href="/login">Sign In</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/register">Sign Up</Link>
+          </Button>
+        </>
+      )}
+
+      {session && (
+        <>
+          {isHome ? (
+            <Button asChild>
+              <Link href="/">Dashboard</Link>
+            </Button>
+          ) : (
+            <>
+              <Button variant="secondary" onClick={handleSignOut}>
+                Sign Out
+              </Button>
+              <Button asChild>
+                <Link href="/home">Go to Home</Link>
+              </Button>
+            </>
+          )}
+        </>
+      )}
+    </>
+  );
+}
