c037eb9b5db399602e45696e34fc0828e9d96b41
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-06-21 20:59:26 +0200

    update middleware functionality

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { getCookieCache } from "better-auth/cookies";
+import { getSessionCookie } from "better-auth/cookies";
 
-export async function middleware(request: NextRequest) {
-  const sessionCookie = await getCookieCache(request);
-  if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/login", request.url));
+export function middleware(request: NextRequest) {
+  const session = getSessionCookie(request);
+
+  if (!session) {
+    if (request.nextUrl.pathname === "/")
+      return NextResponse.redirect(new URL("/home", request.url));
+    else return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  // TODO: Implement route matching regex
-  matcher: [],
+  matcher: ["/", "/profile/:path*"],
 };
